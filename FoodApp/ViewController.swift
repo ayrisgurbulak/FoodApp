a26e63891dd0337d582b04a55efca2f5c4248efb
Ayris Gürbulak <ayriisgr@gmail.com>
2022-05-19 11:59:17 +0300

    created

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Ayris Gürbulak on 19.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
